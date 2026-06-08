64d00837797f63813bf4d6043291398a9ced08de
rahulgopal025 <rahulgopal025@gmail.com>
2026-06-08 14:00:07 +0530

    Add problem details route

--- backend/routes/problem.routes.js
+++ backend/routes/problem.routes.js
@@ -2,14 +2,32 @@ const express = require("express");
 const router = express.Router();
 const Problem = require("../models/Problem");
 
-
 router.get("/", async (req, res) => {
   try {
-    const problems = await Problem.find({}); 
-    console.log("Found Problems:", problems.length); 
-    res.json(problems); 
+    const problems = await Problem.find({});
+    console.log("Found Problems:", problems.length);
+    res.json(problems);
   } catch (err) {
     res.status(500).json({ message: err.message });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const problem = await Problem.findById(req.params.id);
+
+    if (!problem) {
+      return res.status(404).json({
+        message: "Problem not found"
+      });
+    }
+
+    res.json(problem);
+
+  } catch (err) {
+    res.status(500).json({
+      message: err.message
+    });
   }
 });
 
